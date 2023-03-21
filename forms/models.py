ade9a957ba6242af6926f9c8fcb135f509cf5fba
onehayzent <sergbartosh@gmail.com>
2023-03-21 13:17:33 +0200

    🗃️ Add models: Form, FormAnswer

--- forms/models.py
+++ forms/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Form(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    description = models.CharField(max_length=255)
+    questions = models.JSONField(null=False)
+    form_id = models.CharField(max_length=64, null=False, unique=True)
+
+
+class FormAnswer(models.Model):
+    form_id = models.ForeignKey(Form, on_delete=models.CASCADE)
+    answer = models.JSONField(null=False)
