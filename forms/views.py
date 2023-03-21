f5ea670e83a2833015271c21679207a4b5fcf97e
onehayzent <sergbartosh@gmail.com>
2023-03-21 17:04:38 +0200

    🚧 Add initial form_view()

--- forms/views.py
+++ forms/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed
 
-# Create your views here.
+
+def form_view(request: HttpRequest) -> HttpResponse:
+    if request.method != "GET":
+        return HttpResponseNotAllowed("GET")
+
+    raise NotImplementedError()
