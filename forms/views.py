d5ded22c5b9460912c0197f0897b000bf5c57892
onehayzent <sergbartosh@gmail.com>
2023-03-21 18:24:38 +0200

    🚧 Finish get all forms API

--- forms/views.py
+++ forms/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed
+from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed, JsonResponse
+
+from forms.models import Form
 
 
 def form_view(request: HttpRequest) -> HttpResponse:
     if request.method != "GET":
         return HttpResponseNotAllowed("GET")
 
-    raise NotImplementedError()
+    forms_query_set = Form.objects.all()
+    forms_json = list(forms_query_set.values())
+
+    return JsonResponse(forms_json, safe=False)
