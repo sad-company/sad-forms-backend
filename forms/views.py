fa2ff6f9858c5927ff6ee42aa02451770f383b80
onehayzent <sergbartosh@gmail.com>
2023-03-23 18:43:22 +0200

    🚧 Add staticmethod get() to remove duplication in form_view_by_form_id()

--- forms/views.py
+++ forms/views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.forms.models import model_to_dict
 from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed, JsonResponse, HttpResponseNotFound
 
@@ -17,18 +16,20 @@ def form_view(request: HttpRequest) -> HttpResponse:
 
 def form_view_by_form_id(request: HttpRequest, form_id: str) -> HttpResponse:
     if request.method == "GET":
-        try:
-            form = Form.objects.get(form_id=form_id)
-        except ObjectDoesNotExist:
+        form = Form.get(form_id)
+
+        if form is None:
             return HttpResponseNotFound()
+
         form_json = model_to_dict(form)
         return JsonResponse(form_json)
 
     if request.method == "DELETE":
-        try:
-            form = Form.objects.get(form_id=form_id)
-        except ObjectDoesNotExist:
+        form = Form.get(form_id)
+
+        if form is None:
             return HttpResponseNotFound()
+
         form.delete()
         return HttpResponse()
 
