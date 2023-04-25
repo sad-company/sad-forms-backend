b5691eadefd38f19dc4aa99b2a0678022975f9b6
onehayzent <sergbartosh@gmail.com>
2023-04-25 21:25:31 +0300

    :construction: Creat form.py and class FormForms and update form_view()  to handle POST request

--- forms/views.py
+++ forms/views.py
@@ -1,17 +1,26 @@
 from django.forms.models import model_to_dict
 from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed, JsonResponse, HttpResponseNotFound
 
-from forms.models import Form
+from .dtos import FormCreateDto
+from .models import Form
 
 
 def form_view(request: HttpRequest) -> HttpResponse:
-    if request.method != "GET":
-        return HttpResponseNotAllowed("GET")
+    if request.method == "GET":
+        forms_query_set = Form.objects.all()
+        forms_json = list(forms_query_set.values())
+        return JsonResponse(forms_json, safe=False)
 
-    forms_query_set = Form.objects.all()
-    forms_json = list(forms_query_set.values())
+    if request.method == "POST":
+        data = FormCreateDto(request.POST)
 
-    return JsonResponse(forms_json, safe=False)
+        if data.is_valid():
+            Form.objects.create(**data.cleaned_data)
+            return HttpResponse()
+
+        return HttpResponse(status=400)
+
+    return HttpResponseNotAllowed("GET", "POST")
 
 
 def form_view_by_form_id(request: HttpRequest, form_id: str) -> HttpResponse:
