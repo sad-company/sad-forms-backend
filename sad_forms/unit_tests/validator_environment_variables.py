6bfaca67b73b50c817629de936b515cb3e4836e8
onehayzent <sergbartosh@gmail.com>
2023-09-28 15:27:17 +0300

    🚧 Add unit tests for validation environment variables

---
+++ sad_forms/unit_tests/validator_environment_variables.py
@@ -0,0 +1,44 @@
+import unittest
+
+from jsonschema.exceptions import ValidationError
+
+from sad_forms.settings import validator_env
+
+
+class TestEnvironmentVariableValidation(unittest.TestCase):
+    def test_when_everything_is_wrong(self):
+        environment = {'DB_HOST': '1', 'DB_PORT': '2', 'DB_NAME': '3',
+                       'DB_USER': '4', 'DB_PASSWORD': '5'}
+        try:
+            validator_env(environment)
+        except ValidationError as e:
+            self.assertRaises(ValidationError)
+
+    def test_when_all_letters(self):
+        environment = {'DB_HOST': 'sadsadsa', 'DB_PORT': 'asdsad', 'DB_NAME': 'cxzczx',
+                       'DB_USER': 'adsad', 'DB_PASSWORD': 'AAAAAA'}
+        try:
+            validator_env(environment)
+        except ValidationError as e:
+            self.assertRaises(ValidationError)
+
+    def test_when_everything_is_empty(self):
+        environment = {'DB_HOST': '', 'DB_PORT': '', 'DB_NAME': '',
+                       'DB_USER': '', 'DB_PASSWORD': ''}
+        try:
+            validator_env(environment)
+        except ValidationError as e:
+            self.assertRaises(ValidationError)
+
+    def test_when_commas(self):
+        environment = {'DB_HOST': '128,0,0,1', 'DB_PORT': '5238', 'DB_NAME': 'postgres',
+                       'DB_USER': 'root', 'DB_PASSWORD': 'root'}
+        try:
+            validator_env(environment)
+        except ValidationError as e:
+            self.assertRaises(ValidationError)
+
+    def test_when_everything_is_correct(self):
+        environment = {'DB_HOST': '128.0.0.1', 'DB_PORT': '5238', 'DB_NAME': 'postgres',
+                       'DB_USER': 'root', 'DB_PASSWORD': 'root'}
+        self.assertIsNone(validator_env(environment))
